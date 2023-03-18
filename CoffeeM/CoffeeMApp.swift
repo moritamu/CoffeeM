5945c23db3fa368955817c5ea1537e48567935ec
morisum <moris@r.sannet.ne.jp>
2023-03-19 08:48:14 +0900

    Initial Commit

---
+++ CoffeeM/CoffeeMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMApp.swift
+//  CoffeeM
+//
+//  Created by MsMacM on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
